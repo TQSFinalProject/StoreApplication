282ff86dc893578cd09987b0cd3c5fcafefff7bb
rospuye <66674322+rospuye@users.noreply.github.com>
2022-06-01 22:36:15 +0100

    user login and register are functional

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useCookies } from 'react-cookie';
+import axios from 'axios'
 import { Link, useParams, useNavigate } from "react-router-dom";
 
 // Components
@@ -18,7 +20,173 @@ import { faArrowLeft } from '@fortawesome/free-solid-svg-icons'
 // CSS
 import SearchBar from '../components/css/SearchBar.css'
 
+const registerEndpoint = 'register';
+const loginEndpoint = 'authenticate';
+
+// INPUT FIELDS VALIDATION
+
+function isAlphaNumeric(str) {
+    let code, i, len
+
+    for (i = 0, len = str.length; i < len; i++) {
+        code = str.charCodeAt(i);
+        if (!(code > 47 && code < 58) && // numeric (0-9)
+            !(code > 64 && code < 91) && // upper alpha (A-Z)
+            !(code > 96 && code < 123)) { // lower alpha (a-z)
+            return false
+        }
+    }
+    return true
+}
+
+function validateUsername(username) {
+    if (username.length > 2 // length is 3+ characters
+        && username.charAt(0).toLowerCase() != username.charAt(0).toUpperCase() // starts with a letter
+        && isAlphaNumeric(username)) // is an alphanumeric string
+    {
+        return true
+    }
+    return false
+}
+
+function validateEmail(email) {
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+        return true
+    }
+    return false
+}
+
+function validatePassword(password) {
+    const lower = /[a-z]/
+    const digit = /[0-9]/
+
+    if (password.length > 4 // length is 5+ characters
+        && lower.test(password) // has at least one lowercase letter
+        && digit.test(password)) // has at least one digit
+    {
+        return true
+    }
+    return false
+}
+
+// LOGIN COMPONENT
+
 function Login() {
+
+    const [cookies, setCookie] = useCookies(['logged_user', 'token'])
+
+    const [loginUsername, setLoginUsername] = useState("");
+    const [loginPassword, setLoginPassword] = useState("");
+
+    const [regUsername, setRegUsername] = useState("");
+    const [regEmail, setRegEmail] = useState("");
+    const [regPassword, setRegPassword] = useState("");
+    const [regConfPassword, setRegConfPassword] = useState("");
+
+    function validateRegForm() {
+        const validUsername = validateUsername(regUsername);
+        const validEmail = validateEmail(regEmail);
+        const validPassword = validatePassword(regPassword);
+        const equalPasswords = (regPassword === regConfPassword);
+
+        return [validUsername, validEmail, validPassword, equalPasswords];
+    }
+
+    function handleRegister() {
+
+        let allGood = true;
+        const registerValidation = validateRegForm();
+
+        if (!registerValidation[0]) {
+            document.getElementById("regUsernameWarning").style.display = 'block'
+            allGood = false;
+        }
+        else {
+            document.getElementById("regUsernameWarning").style.display = 'none'
+        }
+
+        if (!registerValidation[1]) {
+            document.getElementById("regEmailWarning").style.display = 'block'
+            allGood = false;
+        }
+        else {
+            document.getElementById("regEmailWarning").style.display = 'none'
+        }
+
+        if (!registerValidation[2]) {
+            document.getElementById("regPasswordWarning").style.display = 'block'
+            allGood = false;
+        }
+        else {
+            document.getElementById("regPasswordWarning").style.display = 'none'
+        }
+
+        if (!registerValidation[3]) {
+            document.getElementById("regConfPasswordWarning").style.display = 'block'
+            allGood = false;
+        }
+        else {
+            document.getElementById("regConfPasswordWarning").style.display = 'none'
+        }
+
+        if (allGood) {
+
+            const newRegister = {
+                // TODO: phone number in register form
+                // TODO: email in post request
+                "name": regUsername,
+                "username": regUsername,
+                "password": regPassword,
+                "phone": 961234567, // TODO: this is not static
+                "cart": {} // TODO: hmmmm
+            }
+
+            axios.post(process.env.REACT_APP_BACKEND_URL + registerEndpoint, newRegister)
+                .then(function (response) {
+                    console.log(response);
+                    if (response.data === 'User registered successfully.') {
+                        // TODO: successful registration
+                        alert('Registration was successful! Proceed to login please :)')
+                    }
+                    else {
+                        alert('There was a problem with your registration. Please try again!')
+                    }
+                })
+
+        }
+
+    }
+
+    function handleLogin() {
+
+        document.getElementById("loginWarning").style.display = 'none'
+
+        const newLogin = {
+            "username": loginUsername,
+            "password": loginPassword,
+        }
+
+        axios.post(process.env.REACT_APP_BACKEND_URL + loginEndpoint, newLogin)
+            .then(function (response) {
+                console.log(response.status);
+
+                if (response.status === 200) {
+                    setCookie('logged_user', loginUsername, { path: '/' })
+                    setCookie('token', response.data.token, { path: '/' }) // , maxAge: '3600'
+                    alert('Login was successful :)')
+                    console.log("SUCCESSFUL LOGIN!")
+                    console.log("cookie user: ", cookies.logged_user)
+                    console.log("cookie token: ", cookies.token)
+                }
+                else {
+                    document.getElementById("loginWarning").style.display = 'block'
+                }
+
+            })
+
+    }
+
+
     return (
         <>
             <GeneralNavbar />
@@ -33,20 +201,23 @@ function Login() {
                         <h4 style={{ marginTop: '5%' }}>SIGN IN</h4>
                         <Form style={{ marginTop: '3%' }}>
                             <Form.Group style={{ width: '20vw' }}>
-                                <Form.Label htmlFor="searchAssignedRider" className="inp">
-                                    <Form.Control type="text" id="searchAssignedRider" placeholder="&nbsp;" />
+                                <Form.Label htmlFor="loginUsernameField" className="inp">
+                                    <Form.Control type="text" id="loginUsernameField" placeholder="&nbsp;" onChange={(e) => { setLoginUsername(e.target.value) }} />
                                     <span className="label">Username</span>
                                     <span className="focus-bg"></span>
                                 </Form.Label>
                             </Form.Group>
                             <Form.Group style={{ width: '20vw' }}>
-                                <Form.Label htmlFor="searchStore" className="inp">
-                                    <Form.Control type="password" id="searchStore" placeholder="&nbsp;" />
+                                <Form.Label htmlFor="loginPasswordField" className="inp">
+                                    <Form.Control type="password" id="loginPasswordField" placeholder="&nbsp;" onChange={(e) => { setLoginPassword(e.target.value) }} />
                                     <span className="label">Password</span>
                                     <span className="focus-bg"></span>
                                 </Form.Label>
+                                <Form.Text className="text-muted" id="loginWarning" style={{ display: 'none' }}>
+                                    Your login credentials are incorrect.
+                                </Form.Text>
                             </Form.Group>
-                            <Button variant="primary" type="submit" style={{ marginTop: '3%' }}>
+                            <Button variant="primary" style={{ marginTop: '3%' }} onClick={() => { handleLogin(); }}>
                                 Submit
                             </Button>
                         </Form>
@@ -56,34 +227,46 @@ function Login() {
                         <h4 style={{ marginTop: '5%' }}>SIGN UP</h4>
                         <Form style={{ marginTop: '3%' }}>
                             <Form.Group style={{ width: '20vw' }}>
-                                <Form.Label htmlFor="searchAssignedRider" className="inp">
-                                    <Form.Control type="text" id="searchAssignedRider" placeholder="&nbsp;" />
+                                <Form.Label htmlFor="regUsernameField" className="inp">
+                                    <Form.Control type="text" id="regUsernameField" placeholder="&nbsp;" onChange={(e) => { setRegUsername(e.target.value) }} />
                                     <span className="label">Username</span>
                                     <span className="focus-bg"></span>
+                                    <Form.Text className="text-muted" id="regUsernameWarning" style={{ display: 'none' }}>
+                                        Your username is invalid.
+                                    </Form.Text>
                                 </Form.Label>
                             </Form.Group>
                             <Form.Group style={{ width: '20vw' }}>
-                                <Form.Label htmlFor="searchAssignedRider" className="inp">
-                                    <Form.Control type="text" id="searchAssignedRider" placeholder="&nbsp;" />
+                                <Form.Label htmlFor="regEmailField" className="inp">
+                                    <Form.Control type="text" id="regEmailField" placeholder="&nbsp;" onChange={(e) => { setRegEmail(e.target.value) }} />
                                     <span className="label">E-mail</span>
                                     <span className="focus-bg"></span>
+                                    <Form.Text className="text-muted" id="regEmailWarning" style={{ display: 'none' }}>
+                                        Your e-mail is invalid.
+                                    </Form.Text>
                                 </Form.Label>
                             </Form.Group>
                             <Form.Group style={{ width: '20vw' }}>
-                                <Form.Label htmlFor="searchStore" className="inp">
-                                    <Form.Control type="password" id="searchStore" placeholder="&nbsp;" />
+                                <Form.Label htmlFor="regPasswordField" className="inp">
+                                    <Form.Control type="password" id="regPasswordField" placeholder="&nbsp;" onChange={(e) => { setRegPassword(e.target.value) }} />
                                     <span className="label">Password</span>
                                     <span className="focus-bg"></span>
+                                    <Form.Text className="text-muted" id="regPasswordWarning" style={{ display: 'none' }}>
+                                        Your password is invalid.
+                                    </Form.Text>
                                 </Form.Label>
                             </Form.Group>
                             <Form.Group style={{ width: '20vw' }}>
-                                <Form.Label htmlFor="searchStore" className="inp">
-                                    <Form.Control type="password" id="searchStore" placeholder="&nbsp;" />
+                                <Form.Label htmlFor="regConfPasswordField" className="inp">
+                                    <Form.Control type="password" id="regConfPasswordField" placeholder="&nbsp;" onChange={(e) => { setRegConfPassword(e.target.value) }} />
                                     <span className="label">Confirm Password</span>
                                     <span className="focus-bg"></span>
+                                    <Form.Text className="text-muted" id="regConfPasswordWarning" style={{ display: 'none' }}>
+                                        Your confirmation password doesn't match your initial password.
+                                    </Form.Text>
                                 </Form.Label>
                             </Form.Group>
-                            <Button variant="primary" type="submit" style={{ marginTop: '3%' }}>
+                            <Button variant="primary" style={{ marginTop: '3%' }} onClick={() => { handleRegister(); }}>
                                 Submit
                             </Button>
                         </Form>
