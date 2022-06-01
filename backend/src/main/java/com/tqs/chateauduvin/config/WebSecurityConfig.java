b5c701bcace1c9c75bdf979e0be86f3717d9c6e7
AfonsoCampos971 <afonso.campos@ua.pt>
2022-06-01 17:22:47 +0100

    added endpoint to validate whether token is still active

--- backend/src/main/java/com/tqs/chateauduvin/config/WebSecurityConfig.java
+++ backend/src/main/java/com/tqs/chateauduvin/config/WebSecurityConfig.java
@@ -40,7 +40,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/authenticate", "/register").permitAll()
+                .antMatchers("/authenticate", "/register", "/validate").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .exceptionHandling().authenticationEntryPoint(unauthorizedEntryPoint).and()
