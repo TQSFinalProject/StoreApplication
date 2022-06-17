7df7050d9f52006b8130b9953d4f44883d0e07df
AfonsoCampos971 <afonso.campos@ua.pt>
2022-06-17 16:47:53 +0100

    Hotfixes

--- backend/src/main/java/com/tqs/chateauduvin/controller/AuthenticationController.java
+++ backend/src/main/java/com/tqs/chateauduvin/controller/AuthenticationController.java
@@ -14,8 +14,10 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -60,7 +62,7 @@ public class AuthenticationController {
     public ResponseEntity<Customer> getCustomerDetails(@RequestHeader("authorization") String auth) {
         String token = auth.split(" ")[1];
         String username = jwtTokenUtil.getUsernameFromToken(token);
-        Customer cust = storeServ.getCustomer(username);
+        Customer cust = storeServ.getCustomerByUsername(username);
         cust.setPassword(null);
         return ResponseEntity.ok().body(cust);
     }
--- backend/src/test/java/com/tqs/chateauduvin/IT/CartMechanismTests.java
+++ backend/src/test/java/com/tqs/chateauduvin/IT/CartMechanismTests.java
@@ -1,9 +1,7 @@
 package com.tqs.chateauduvin.IT;
 
 import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
